24e52e61b2dc1d0171f844261672c6734ea4538b
Wil Hammond <wil.hammond@gmail.com>
2023-03-10 16:01:20 -0600

    Ticket view

--- goldenguestsapi/views/ticket_view.py
+++ goldenguestsapi/views/ticket_view.py
@@ -2,39 +2,41 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from goldenguestsapi.models import GoldenGuest, Opponent, Ticket, OrgTicket
+from goldenguestsapi.models import GoldenGuest, Opponent, Ticket
+
 
 class TicketView(ViewSet):
-
 
     def retrieve(self, request, pk):
         ticket = Ticket.objects.get(pk=pk)
         serializer = TicketSerializer(ticket)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-
     def list(self, request):
         ticket = Ticket.objects.all()
         serializer = TicketSerializer(ticket, many=True)
         return Response(serializer.data)
-    
+
     def create(self, request):
 
-        genre = Genre.objects.get(pk=request.data["genre"])
+        goldenguest = GoldenGuest.objects.get(pk=request.data["GoldenGuest"])
+        opponent = Opponent.objects.get(pk=request.data["Opponent"])
 
         ticket = Ticket.objects.create(
-        name=request.data["name"],
-        description=request.data["description"],
-        genre=genre
-    )
+            section=request.data["section"],
+            number_of_tickets=request.data["number_of_tickets"],
+            date=request.data["date"],
+            goldenguest=goldenguest,
+            opponent=opponent
+        )
+
         serializer = TicketSerializer(ticket)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    
 
 
 class TicketSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Ticket
-        fields = ('id', 'section', 'number_of_tickets', 'date', 'goldenguest', 'opponent' )
-        depth= 1
+        fields = ('id', 'section', 'number_of_tickets', 'date', 'goldenguest', 'opponent')
+        depth = 1
